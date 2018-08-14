54622b51047edc713a96ba21734f9ccd0f99d222
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2018-08-14 10:28:38 +0200

    Typos

--- Longwatch.py
+++ Longwatch.py
@@ -6,17 +6,17 @@ import time
 
 def main():
     db = DobermanDB.DobermanDB()
-    parser = argparse.ArgumentParser('%(prog)s: like tail -f but for Doberman messages')
+    parser = argparse.ArgumentParser(usage='%(prog)s: like tail -f but for Doberman messages')
     parser.add_argument('--delay', type=int, default=5,
                         help='How often to refresh (in seconds)')
-    parser.add_argument('--level', type=int, options=range(10,60,10), default=20,
+    parser.add_argument('--level', type=int, choices=range(10,60,10), default=20,
                         help='Minimum level of message to display')
     args = parser.parse_args()
     msg_format = '{when} | {level} | {name} | {funcname} | {lineno} | {msg}'
     try:
         while True:
             then = datetime.datetime.now() - timedelta(seconds = args.delay)
-            cursor = db.readFromDatabase('logging','logs', {'when' : {'$gt' : then}, 'level' : {'$geq' : args.level}})
+            cursor = db.readFromDatabase('logging','logs', {'when' : {'$gt' : then}, 'level' : {'$gte' : args.level}})
             for row in cursor:
                 print(msg_format.format(**row))
             time.sleep(args.delay)
