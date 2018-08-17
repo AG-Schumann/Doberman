1f90981963707d99e1e508e12a7e340e2742068b
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2018-08-17 10:47:45 +0200

    Switched Pfeiffer from serial to LAN

--- pfeiffer_tpg.py
+++ pfeiffer_tpg.py
@@ -1,18 +1,23 @@
-from ControllerBase import SerialController
+from ControllerBase import LANController
 import re  # EVERYBODY STAND BACK xkcd.com/208
 from utils import number_regex
 
 
-class pfeiffer_tpg(SerialController):
+class pfeiffer_tpg(LANController):
     def __init__(self, opts):
         self._msg_begin = ''
-        self._msg_end = '\r\n\x05\r\n'
+        self._msg_end = '\r\n\x05'
         super().__init__(opts)
         self.commands = {
                 'identify' : 'AYT',
-                'read' : 'PRX',
+                'read' : 'PR1',
                 }
-        self.read_command = re.compile(r'[0-9],(?P<value>%s)' % number_regex)
+        self.read_command = re.compile(r'(?P<status>[0-9]),(?P<value>%s)' % number_regex)
+
+    def _getControl(self):
+        super()._getControl()
+        self.SendRecv(self.commands['AYT'])
+        # stops the continuous flow of data
 
     def Readout(self):
         resp = self.SendRecv(self.commands['read'])
@@ -22,7 +27,7 @@ class pfeiffer_tpg(SerialController):
         if not m:
             self.logger.debug('Error?')
             return {'retcode' : -3, 'data' : -1}
-        return {'retcode' : 0, 'data' : float(m.group('value'))}
+        return {'retcode' : int(m.group('status')), 'data' : float(m.group('value'))}
 
     def isThisMe(self, dev):
         pass
