9e9a69ef1a9e5f71e00f82d4b06be1627764d305
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2018-10-30 13:35:29 +0100

    Update example controller

--- ExampleController.py
+++ ExampleController.py
@@ -1,43 +1,42 @@
 from BaseController import SerialController  # or LANController, if applicable
-import logging  # you need this
+import re  # if you want to accept commands
 
 
 class ExampleController(SerialController):
     """
     An example of how to make your own controller
     """
-
+    accepted_commands = [
+            "a pattern: a description of what this command does",
+        ] # this object is used if you ask for help from the command line
     def __init__(self, opts):
-        self.logger = logging.getLogger(__name__) # instantiated here so log messages include controller name
         self._msg_start = ''  # whatever character(s) messages start with
         self._msg_end = '\r\n'  # same, for end
-        self.some_special_parameter = opts.additional_params['special_parameter']
         self.commands = {'read' : 'command',
                          'also_read' : 'also_command',
                          'check_id' : 'check_id',
+                         'set', 'set_value',
                          }
-        super().__init__(opts, logger)  # calls SerialController.__init__, which calls Controller.__init__
-        # quantities like the vendorID, ip address, etc are all pulled from opts further up.
-        # You only need to pull device-specific quantities here
+        super().__init__(opts)  # calls SerialController.__init__, which calls Controller.__init__
+        # if you need any quantities from the config doc in the command dictionary, call init first
 
-    def checkController(self):
+        self.command_patterns = [
+                (re.compile('a pattern'), lambda x : self.commands['set'].format(**x.groupdict())),
+                ]
+        # this is a list of (regular expression, function) objects. The regular expression
+        # matches the commmand issued via the command line, and the function takes as
+        # argument a re.match object and returns the string to the sent to the controller
+
+    def isThisMe(self, dev):
         """
-        This function makes sure you connected to the correct controller
+        This function checks to see if the device 'dev' is, in fact, this controller.
+        This is only necessary if you inherit from SerialController.
         """
-        resp = self.SendRecv(self.commands['check_id'])
-        if resp['retcode']:
-            self.logger.error('An error')
-            self._connected = False
-            return -1
+        resp = self.SendRecv(self.commands['check_id'], dev)
+        if resp['retcode'] or not ret['data']:
+            return False
         if resp['data'] == self.some_uniquely_identifying_quantity:
-            self.add_ttyUSB()  # only for serial controllers
-            return 0
-        else:
-            # connected to wrong controller
-            self.logger.error('Connected to wrong controller')
-            self._connected = False
-            return -2
-        return -3
+            True
 
     def Readout(self):
         """
@@ -48,26 +47,11 @@ class ExampleController(SerialController):
         status = []
         for coms in ['read','also_read']:
             resp = self.SendRecv(self.commands[com])
-            if resp['retcode']:
-                status.append(-1)
+            if resp['retcode'] or not resp['data']:
+                status.append(resp['retcode'])
                 vals.append(-1)
             else:
                 status.append(0)
                 vals.append(float(resp['data']))
         return {'retcode' : status, 'data' : vals}
-
-    def ExecuteCommand(self, command):
-        """
-        Executes the specified command. For example:
-        itrip <value>
-        valve open
-        Regular expressions might be useful for parsing the input
-        """
-        task, value = command.split()
-        com = self.setcommand.format(cmd=self.commands[task],value=value)
-        resp = self.SendRecv(com)
-        if resp['retcode']:
-            self.logger.error('Could not send command %s' % command)
-        else:
-            self.logger.debug('Successfully sent command %s' % command)
 
