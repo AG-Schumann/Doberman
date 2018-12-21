7f7767579fa19100d2902732ddf97425488494d9
Darryl Masson <darrylmasson@gmail.com>
2018-12-21 10:50:40 +0100

    Integrating pulser

---
+++ rigol_dg1022.py
@@ -0,0 +1,9 @@
+from ControllerBase import SerialController
+import re  # EVERYBODY STAND BACK xkcd.com/207
+
+
+class rigol_1022(SerialController):
+    """
+    Pulser for the LED calibration
+    """
+
