b0dc471584003582483a2ee88b84bfed1cff33f1
Darryl Masson <darrylmasson@gmail.com>
2019-02-08 18:07:34 +0100

    A few more changes

--- labjack.py
+++ labjack.py
@@ -23,12 +23,7 @@ class labjack(Controller):
         self._getControl()
 
     def NTCtoTemp(self, val):
-        # rc (old) = [5 10e3 8.181e-6 11.67e-6 1000]
-        # rc (simplified) = [10 5.1167 -1.08181]
-        # tc = [77.69 -9.562 0.545 -0.1183]
         resistance = self.rc[0]*val/(self.rc[1] + self.rc[2]*val)
-        #resistance = val/(((self.rc[0]-val)/self.rc[1])-\
-        #        (self.rc[2]*val)+self.rc[3])/self.rc[4]
         temp = sum([v*resistance**i for i,v in enumerate(self.tc)])
         return temp
 
@@ -72,31 +67,3 @@ class labjack(Controller):
             value = v['state']
             retcode = 0
         callback((index, time.time(), value, retcode))
-
-    def Readout(self):
-        voltage = [None]*len(self.analog_channels)
-        overvolt = [None]*len(self.analog_channels)
-        state = [None]*len(self.digital_channels)
-
-        for ch in self.analog_channels:
-            v = self._device.eAnalogIn(channel=ch, gain=0, **self.read_args)
-            voltage[ch] = v['voltage']
-            overvolt[ch] = v['overVoltage']
-        for ch in self.digital_channels:
-            v = self._device.eDigitalIn(channel=ch, readD=0, **self.read_args)
-            state[ch] = v['state']
-
-        count = self._device.eCount(resetCounter=1)
-        counts = count['count']
-        now = count['ms']
-        status = [0,0,0,0,0]
-        if now == self.then:
-            freq = -1
-            status[2] = -3
-        else:
-            freq = counts/(now - self.then)*1000
-            self.then = now
-        data = [voltage[2], self.NTCtoTemp(voltage[0]), freq, state[0], state[1]]
-
-        return {'retcode' : status, 'data' : data}
-
