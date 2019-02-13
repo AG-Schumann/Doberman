878d911bc084f76202d3af36cf4850cc5471a304
Darryl Masson <darrylmasson@gmail.com>
2019-02-13 09:45:45 +0100

    Test sensor now integrated

--- TestSensor.py
+++ TestSensor.py
@@ -1,5 +1,4 @@
 import socket
-import signal
 import time
 import random
 import argparse
@@ -10,20 +9,13 @@ class TestSensorLAN(object):
         self.dev = socket.socket()
         self.dev.bind(('localhost',5000))
         self.dev.listen(1)
-        self.interrupted = False
-        signal.signal(signal.SIGINT, self.interrupt)
-        signal.signal(signal.SIGTERM, self.interrupt)
-
-    def interrupt(self, *args):
-        print('Interrupted')
-        self.interrupted = True
 
     def sandwich(self):
-        while not self.interrupted:
+        while True:
             client, _ = self.dev.accept()
             msg = client.recv(1024)
-            print('Received %s' % msg.decode())
-            sleep_for = 0.0095*random.random()
+            print('Received %s' % msg)
+            sleep_for = 0.006*random.random()
             print('Processing time: %.3g' % sleep_for)
             time.sleep(sleep_for)
             if b'one' in msg:
@@ -32,8 +24,8 @@ class TestSensorLAN(object):
                 client.sendall(b'OK;%.3g\r\n' % random.randint(1,6))
             else:
                 client.sendall(b'ERR;01\r\n')
-            client.close()
-            sleep_for = 0.1*random.random()
+            #client.close()
+            sleep_for = 0.05*random.random()
             print('Cleanup time: %.3g' % sleep_for)
             time.sleep(sleep_for)
 
