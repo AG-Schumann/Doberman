d929f918c65897bd0f3dc8b5b4b7f786256675d0
Darryl Masson <darrylmasson@gmail.com>
2019-02-19 17:45:17 +0100

    No more hard-coded paths

--- Plugin.py
+++ Plugin.py
@@ -22,7 +22,7 @@ def main(db):
                         help='Which run mode to use', default='default')
     args = parser.parse_args()
 
-    plugin_paths=['.']
+    plugin_paths=[utils.doberman_dir]
     logger = logging.getLogger(args.plugin_name)
     logger.addHandler(DobermanLogging.DobermanLogger(db))
     loglevel = db.getDefaultSettings(runmode=args.runmode,name='loglevel')
