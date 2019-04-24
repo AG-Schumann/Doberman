f05d1e41d70f7207e51b939841cab2171dd5b192
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2019-04-24 14:24:26 +0200

    Changed Pfeiffer to Serial

--- pfeiffer_tpg.py
+++ pfeiffer_tpg.py
@@ -1,9 +1,9 @@
-from SensorBase import LANSensor
+from SensorBase import SerialSensor
 import re  # EVERYBODY STAND BACK xkcd.com/208
 from utils import number_regex
 
 
-class pfeiffer_tpg(LANSensor):
+class pfeiffer_tpg(SerialSensor):
     def SetParameters(self):
         self._msg_begin = ''
         self._msg_end = '\r\n\x05'
@@ -18,3 +18,10 @@ class pfeiffer_tpg(LANSensor):
         self.SendRecv(self.commands['identify'])
         # stops the continuous flow of data
 
+    def isThisMe(self, dev):
+        ret = self.SendRecv(self.commands['identify'], dev)
+        if ret['retcode'] or ret['data'] is None:
+            return False
+        if self.serialID in ret['data'].decode():
+            return True
+        return False
