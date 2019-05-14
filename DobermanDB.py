4cfbdf3fde54ead68910125590cb446f1a136f83
Darryl Masson <darrylmasson@gmail.com>
2019-05-14 15:11:23 +0200

    Displays log messages after submitting a command

--- DobermanDB.py
+++ DobermanDB.py
@@ -359,6 +359,12 @@ class DobermanDB(object):
             m = re.search(pattern, command_str)
             if m:
                 self.ProcessCommandStepTwo(m, user=user)
+                time.sleep(3)
+                for log in self.db.readFromDatabase('logging', 'logs',
+                        cuts={'when':
+                            {'$gte': dtnow()-datetime.timedelta(seconds=3)}},
+                        sort=([('_id', -1)])):
+                    print('{when} | {level} | {name} | {msg}'.format(**log))
                 break
         else:
             print('Command \'%s\' not understood' % command_str)
