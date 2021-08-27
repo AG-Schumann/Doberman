2b8480c8814f27337c2daab511b85684a095fe6c
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2021-08-27 12:00:39 +0200

    Work

--- Doberman/BaseSensor.py
+++ Doberman/BaseSensor.py
@@ -88,16 +88,17 @@ class Sensor(object):
         access (ie, the isThisMe routine avoids this)
         """
         self.logger.debug('Readout scheduler starting')
-        while not self.event.is_set():
-            try:
-                command, retq = self.cmd_queue.get(timeout=0.001)
-                self.logger.debug(f'Executing {command}')
-                ret = self.send_recv(command)
-                self.cmd_queue.task_done()
-                if retq is not None:
-                    retq.put(ret)
-            except queue.Empty:
-                pass
+        with self.cv:
+            while not self.event.is_set():
+                self.cv.wait_for(lambda: (len(self.cmd_queue > 0) || self.event.is_set()))
+                if len(self.cmd_queue) > 0:
+                    command, (retq, retcv) = self.cmd_queue.pop(index=0)
+                    self.cmd_queue_lock.release()
+                    ret = self.send_recv(command)
+                    if retq is not None:
+                        with retcv:
+                            retq.put(ret)
+                            retcv.notify()
         self.logger.debug('Readout scheduler returning')
 
     def add_to_schedule(self, reading_name=None, command=None, retq=None):
@@ -115,11 +116,15 @@ class Sensor(object):
         if reading_name is not None:
             if retq is None:
                 return
-            self.logger.debug(f'Scheduling {self.readings[reading_name]}')
-            self.cmd_queue.put((self.readings[reading_name], retq))
+            with self.cv:
+                self.logger.debug(f'Scheduling {self.readings[reading_name]}')
+                self.cmd_queue.append((self.readings[reading_name], retq))
+                self.cv.notify()
         elif command is not None:
-            self.logger.debug(f'Scheduling {command}')
-            self.cmd_queue.put((command, None))
+            with self.cv:
+                self.logger.debug(f'Scheduling {command}')
+                self.cmd_queue.append((command, (None,None))
+                self.cv.notify()
         return
 
     def process_one_reading(self, name=None, data=None):
