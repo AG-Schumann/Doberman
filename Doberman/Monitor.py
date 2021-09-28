633b1948e9d13167a298b4b07553b5bc6b0eefa3
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2021-09-28 11:27:05 +0200

    Lots of good changes

--- Doberman/Monitor.py
+++ Doberman/Monitor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python3
 import Doberman
 from pymongo import MongoClient
 import argparse
@@ -14,7 +14,7 @@ def main(mongo_client):
     group.add_argument('--alarm', action='store_true', help='Start the alarm monitor')
     group.add_argument('--host', action='store_true', help='Start this host\'s monitor')
     group.add_argument('--sensor', help='Start the specified sensor monitor')
-    group.add_argument('--pipeline', action='store_true', help='Start the pipeline monitor')
+    group.add_argument('--pipeline', help='Start a pipeline')
     group.add_argument('--status', action='store_true', help='Current status snapshot')
     args = parser.parse_args()
 
@@ -54,7 +54,7 @@ def main(mongo_client):
         if 'Test' in args.sensor:
             db.experiment_name = 'testing'
     elif args.pipeline:
-        kwargs['name'] = 'pipeline_monitor'
+        kwargs['name'] = 'pipeline_' + args.pipeline
         ctor = Doberman.PipelineMonitor
     elif args.status:
         pprint.pprint(db.get_current_status())
--- Doberman/PipelineMonitor.py
+++ Doberman/PipelineMonitor.py
@@ -4,18 +4,13 @@ __all__ = 'PipelineMonitor'.split()
 
 class PipelineMonitor(Doberman.Monitor):
     """
-    A subclass to handle pipelines. THERE SHOULD ONLY BE ONE
+    A subclass to handle a pipeline. Only one pipeline per pipeline monitor to make it easier to restart
+    during development.
     """
 
     def setup(self):
-        self.pipelines = {}
-
-    def shutdown(self):
-        pass
-
-    def start_pipeline(self, name):
-        doc = self.db.get_pipeline(name)
-        if doc is None:
+        name = self.name.split('_', maxsplit=1)[1]
+        if (doc := self.db.get_pipeline(name)) is None:
             self.logger.error(f'No pipeline named {name} found!')
             return -1
         p = Doberman.Pipeline(db=self.db, logger=self.logger, name=name)
@@ -24,35 +19,17 @@ class PipelineMonitor(Doberman.Monitor):
         except Exception as e:
             self.logger.error(f'Error building pipeline {name}: {e}')
             return -1
-        self.pipelines[name] = p
         self.register(obj=p.process_cycle, period=doc['period'], name=name)
-
-    def stop_pipeline(self, name):
-        self.stop_thread(name)
-        del self.pipelines[name]
+        self.pipeline = p
 
     def handle_commands(self):
-        doc = self.db.find_command(self.name)
-        while doc is not None:
+        while (doc := self.db.find_command(self.name)) is not None:
             self.logger.debug(f'Found command: {doc["command"]}')
             try:
-                if ' ' in doc['command']:
-                    command, target = doc['command'].split()
-                    if command == 'pipelinectl_start':
-                        self.start_pipeline(target)
-                    elif command == 'pipelinectl_stop':
-                        self.stop_pipeline(target)
-                    elif command == 'pipelinectl_restart':
-                        self.stop_pipeline(target)
-                        self.start_pipeline(target)
-                    else:
-                        self.logger.error(f'Bad command: {doc["command"]}')
-                else:
-                    command = doc['command']
-                    if command == 'stop':
-                        self.sh.event.set()
-                        return
+                command = doc['command']
+                if command == 'stop':
+                    self.sh.event.set()
+                    return
             except Exception as e:
                 self.logger.error(f'Received malformed command: {doc["command"]}')
-            doc = self.db.find_command(self.name)
 
