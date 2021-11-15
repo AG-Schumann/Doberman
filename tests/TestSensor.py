5c478d8c8240d74362326f1b58d1b253c644efcf
Jaron Grigat <jarongrigat@Jarons-MacBook-Pro.fritz.box>
2021-11-15 14:09:26 +0100

    ocd

--- tests/TestSensor.py
+++ tests/TestSensor.py
@@ -1,4 +1,4 @@
-import Doberman
+from Doberman import utils
 import re
 
 
@@ -6,11 +6,11 @@ class TestSensor(Doberman.TestSensor):
     def set_parameters(self):
         self._msg_start = '*'
         self._msg_end = '\r\n'
-        self.reading_commands = {'t_in' : 'READ:t_in',
-                                 't_amb' : 'READ:t_amb',
+        self.reading_commands = {'t_in': 'READ:t_in',
+                                 't_amb': 'READ:t_amb',
                                  'q_add': 'READ:q_add'}
-        self.reading_pattern = re.compile(f'OK;(?P<value>{Doberman.utils.number_regex})'.encode())
-        self.command_pattern = re.compile(f'set q_add ({Doberman.utils.number_regex})')
+        self.reading_pattern = re.compile(f'OK;(?P<value>{utils.number_regex})'.encode())
+        self.command_pattern = re.compile(f'set q_add ({utils.number_regex})')
 
     def execute_command(self, command):
         if (m := self.command_pattern.match(command)) is not None:
