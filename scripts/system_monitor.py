56a93438f8b5747a8cd3602c3b77e71f285e7e7f
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2021-12-09 11:44:03 +0100

    Still don't understand scope in python

--- scripts/system_monitor.py
+++ scripts/system_monitor.py
@@ -5,79 +5,82 @@ import time
 from pymongo import MongoClient
 import os
 
-period = 2
-n_cpus = psutil.cpu_count()
-# ignore nics and disks that look like these:
-ignore_nics = ['lo']
-ignore_disks = ['loop', 'ram', 'boot']
 
-# setup disk and network rate caching
-net_io = psutil.net_io_counters(True)
-last_recv = 0
-last_sent = 0
-for nic in net_io:
-    if any([n in nic for n in ignore_nics]):
-        continue
-    last_recv += net_io[nic].bytes_recv
-    last_sent += net_io[nic].bytes_sent
-disk_io = psutil.disk_io_counters(True)
-last_read = 0
-last_write = 0
-for disk in disk_io:
-    if any([d in disk for d in ignore_disks]):
-        continue
-    last_read += disk_io[disk].read_bytes
-    last_write += disk_io[disk].write_bytes
+class Sysmon(object):
+    def __init__(self):
+        self.period = 2
+        self.n_cpus = psutil.cpu_count()
+        # ignore nics and disks that look like these:
+        self.ignore_nics = ['lo']
+        self.ignore_disks = ['loop', 'ram', 'boot']
 
-def monitor():
-    load1, load5, load15 = psutil.getloadavg()
-    fields = {'load1': load1 / n_cpus, 'load5': load5 / n_cpus, 'load15': load15 / n_cpus}
-    cpu_percent = sorted(psutil.cpu_percent(percpu=True))
-    fields['cpu0'] = cpu_percent[-1]
-    fields['cpu1'] = cpu_percent[-2] if n_cpus > 1 else 0.
+        # setup disk and network rate caching
+        net_io = psutil.net_io_counters(True)
+        self.last_recv = 0
+        self.last_sent = 0
+        for nic in net_io:
+            if any([n in nic for n in ignore_nics]):
+                continue
+            self.last_recv += net_io[nic].bytes_recv
+            self.last_sent += net_io[nic].bytes_sent
+        disk_io = psutil.disk_io_counters(True)
+        self.last_read = 0
+        self.last_write = 0
+        for disk in disk_io:
+            if any([d in disk for d in ignore_disks]):
+                continue
+            self.last_read += disk_io[disk].read_bytes
+            self.last_write += disk_io[disk].write_bytes
 
-    mem = psutil.virtual_memory()
-    fields['mem_pct'] = mem.percent
-    swap = psutil.swap_memory()
-    fields['swap_pct'] = swap.percent
+    def monitor(self):
+        load1, load5, load15 = psutil.getloadavg()
+        fields = {'load1': load1 / self.n_cpus, 'load5': load5 / self.n_cpus, 'load15': load15 / self.n_cpus}
+        cpu_percent = sorted(psutil.cpu_percent(percpu=True))
+        fields['cpu0'] = cpu_percent[-1]
+        fields['cpu1'] = cpu_percent[-2] if self.n_cpus > 1 else 0.
 
-    temp_dict = psutil.sensors_temperatures()
-    if 'coretemp' in temp_dict.keys():
-        for row in temp_dict['coretemp']:
-            if 'Package' in row.label:  # Fujitsu Intel core servers
-                socket = row.label[-1]  # max 10 sockets per machine
-                fields[f'cpu_{socket}_temp'] = row.current
-    elif len(temp_dict) == 1:
-        key = list(temp_dict.keys())[0]
-        fields['cpu_0_temp'] = temp_dict[key][0].current
-    else:
-        print(f'Couldn\'t read out CPU temperatures')
-        fields['cpu_0_temp'] = -1.
-    net_io = psutil.net_io_counters(True)
-    this_recv = 0
-    this_sent = 0
-    for nic in net_io:
-        if any([n in nic for n in ignore_nics]):
-            continue
-        this_recv += net_io[nic].bytes_recv
-        this_sent += net_io[nic].bytes_sent
-    fields['network_recv'] = ((this_recv - last_recv)>>10)/period
-    fields['network_sent'] = ((this_sent - last_sent)>>10)/period
-    last_recv = this_recv
-    last_sent = this_sent
-    disk_io = psutil.disk_io_counters(True)
-    this_read = 0
-    this_write = 0
-    for disk in disk_io:
-        if any([d in disk for d in ignore_disks]):
-            continue
-        this_read += disk_io[disk].read_bytes
-        this_write += disk_io[disk].write_bytes
-    fields['disk_read'] = ((this_read - last_read)>>10) /period
-    fields['disk_write'] = ((this_write - last_write)>>10) /period
-    last_read = this_read
-    last_write = this_write
-    return fields
+        mem = psutil.virtual_memory()
+        fields['mem_pct'] = mem.percent
+        swap = psutil.swap_memory()
+        fields['swap_pct'] = swap.percent
+
+        temp_dict = psutil.sensors_temperatures()
+        if 'coretemp' in temp_dict.keys():
+            for row in temp_dict['coretemp']:
+                if 'Package' in row.label:  # Fujitsu Intel core servers
+                    socket = row.label[-1]  # max 10 sockets per machine
+                    fields[f'cpu_{socket}_temp'] = row.current
+        elif len(temp_dict) == 1:
+            key = list(temp_dict.keys())[0]
+            fields['cpu_0_temp'] = temp_dict[key][0].current
+        else:
+            print(f'Couldn\'t read out CPU temperatures')
+            fields['cpu_0_temp'] = -1.
+        net_io = psutil.net_io_counters(True)
+        this_recv = 0
+        this_sent = 0
+        for nic in net_io:
+            if any([n in nic for n in self.ignore_nics]):
+                continue
+            this_recv += net_io[nic].bytes_recv
+            this_sent += net_io[nic].bytes_sent
+        fields['network_recv'] = ((this_recv - self.last_recv)>>10)/self.period
+        fields['network_sent'] = ((this_sent - self.last_sent)>>10)/self.period
+        self.last_recv = this_recv
+        self.last_sent = this_sent
+        disk_io = psutil.disk_io_counters(True)
+        this_read = 0
+        this_write = 0
+        for disk in disk_io:
+            if any([d in disk for d in self.ignore_disks]):
+                continue
+            this_read += disk_io[disk].read_bytes
+            this_write += disk_io[disk].write_bytes
+        fields['disk_read'] = ((this_read - self.last_read)>>10) /self.period
+        fields['disk_write'] = ((this_write - self.last_write)>>10) /self.period
+        self.last_read = this_read
+        self.last_write = this_write
+        return fields
 
 class DumbLogger(object):
     def __init__(self):
@@ -91,10 +94,11 @@ def main(client):
     db = Doberman.Database(client, experiment_name=os.environ['DOBERMAN_EXPERIMENT_NAME'],
             bucket_override='sysmon')
     db.logger = DumbLogger() # in case we need to print a message
+    sm = Sysmon()
     tags = {'host': db.hostname}
     while sh.run:
         try:
-            fields = monitor()
+            fields = sm.monitor()
             db.write_to_influx(topic='sysmon', tags=tags, fields=fields)
         except Exception as e:
             print(f'Caught a {type(e)}: {e}')
