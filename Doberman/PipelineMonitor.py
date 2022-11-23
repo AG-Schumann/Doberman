886826e1919ca128529c9ecf6b340307a212be33
Jaron Grigat <jarongrigat@phy-10-126-165-234.eduroam-phy.privat>
2022-11-23 13:41:05 +0100

    small fix

--- Doberman/PipelineMonitor.py
+++ Doberman/PipelineMonitor.py
@@ -32,8 +32,8 @@ class PipelineMonitor(Doberman.Monitor):
         if (doc := self.db.get_pipeline(name)) is None:
             self.logger.error(f'No pipeline named {name} found')
             return
-        if (doc['status'] != 'inactive'):
-            self.logger.debug(f'The pipeline named {name} is already started')
+        if name in self.pipelines:
+            self.logger.debug(f'I already manage a pipeline called {name}')
             return
         self.logger.debug(f'starting pipeline {name}')
         self.db.set_pipeline_value(name, [('status', 'active')])
